c5e33b553c9610c2b3bf2d5ab0621877003ad588
Sofiane Daouzli <sofianesk8@hotmail.com>
2019-12-15 21:03:05 +0100

    index view works with actors list from DB

--- stars/views.py
+++ stars/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import Actor
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the stars index.")
+    actors = Actor.objects.all()
+    template = loader.get_template('stars/index.html')
+    context = {
+        'actors': actors
+    }
+    return HttpResponse(template.render(context, request))
+
+def list(request):
+    return HttpResponse()
